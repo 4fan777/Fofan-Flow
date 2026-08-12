abe8ba2c60025575563bf627eb30f1db2e89161c
4fan777 <kartinkinapolina@gmail.com>
2026-08-12 23:01:47 +0500

    Add exact pricing and 3-day trial offer

---
+++ miniapp/pricing.js
@@ -0,0 +1,4 @@
+(function(global){
+  const matrix={basic:{1:380,3:1080,6:2050,12:3870},pro:{1:490,3:1390,6:2640,12:4990},unlimit:{1:780,3:2220,6:4210,12:7950}};
+  global.FM_PRICES=Object.freeze(matrix);
+})(globalThis);
