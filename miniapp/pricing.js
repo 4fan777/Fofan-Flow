eb3e80fcb45aa5d9046f97a33287866d0fa59866
4fan777 <kartinkinapolina@gmail.com>
2026-08-16 19:51:35 +0500

    Add monthly sale prices and promo display

--- miniapp/pricing.js
+++ miniapp/pricing.js
@@ -1,4 +1,6 @@
 (function(global){
-  const matrix={basic:{1:380,3:1080,6:2050,12:3870},pro:{1:490,3:1390,6:2640,12:4990},unlimit:{1:780,3:2220,6:4210,12:7950}};
+  const matrix={basic:{1:270,3:1080,6:2050,12:3870},pro:{1:340,3:1390,6:2640,12:4990},unlimit:{1:550,3:2220,6:4210,12:7950}};
+  const original={basic:{1:380},pro:{1:490},unlimit:{1:780}};
   global.FM_PRICES=Object.freeze(matrix);
+  global.FM_ORIGINAL_PRICES=Object.freeze(original);
 })(globalThis);
