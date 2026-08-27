3beeb22f6b7eff57ef91e0a082a183c1e7ce748c
4fan777 <kartinkinapolina@gmail.com>
2026-08-28 00:54:47 +0500

    Update subscription pricing and plan names

--- miniapp/pricing.js
+++ miniapp/pricing.js
@@ -1,6 +1,7 @@
 (function(global){
-  const matrix={basic:{1:270,3:1080,6:2050,12:3870},pro:{1:340,3:1390,6:2640,12:4990},unlimit:{1:550,3:2220,6:4210,12:7950}};
-  const original={basic:{1:380},pro:{1:490},unlimit:{1:780}};
+  const matrix={basic:{1:490,3:1400,6:2650,12:5000},pro:{1:780,3:2220,6:4210,12:7960},unlimit:{1:1750,3:4990,6:9450,12:17850}};
+  const original={basic:{1:980},pro:{1:1560},unlimit:{1:3500}};
   global.FM_PRICES=Object.freeze(matrix);
   global.FM_ORIGINAL_PRICES=Object.freeze(original);
+  global.FM_SALE_PERCENT=50;
 })(globalThis);
